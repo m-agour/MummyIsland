dd1d62cf8397a55a869e893c11593756f8dad4de
m-agour <mo.aggour@gmail.com>
2021-06-29 09:56:31 +0200

    modified some files

--- noise.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from PIL import Image
-
-SHAPE = (256, 256)
-
-noise = np.random.normal(255./2,255./10,SHAPE)
-image = Image.fromarray(noise)
-image = image.convert('RGB')
-image.save("gaussian_noise.png")
